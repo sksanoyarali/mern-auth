c0492ee89e300c1346e5acbc429bcbc78e754bee
SK sanoyar ali <asksanoyar33@gmail.com>
2025-10-13 23:47:54 +0530

    added routing for different pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import EmailVerify from './pages/EmailVerify'
+import Login from './pages/Login'
+import ResetPassword from './pages/ResetPassword'
 
 const App = () => {
-  return <div className="">App</div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
